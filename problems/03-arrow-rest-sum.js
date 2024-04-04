3b0890b43e6067fb4dd372ec610db28b7f1b2a87
robweekes <weekes.robert@gmail.com>
2024-04-04 16:35:33 -0700

    test comment 3

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -3,7 +3,7 @@ Write a function named `arrowRestSum` that accepts all incoming parameters
 and sums them. Assign the function to a variable using the const keyword and
 arrow function syntax.
 
-**Hint**: Use rest parameter syntax!
+**Hint**: Use rest parameter syntax!   // test comment
 
 Examples:
 arrowRestSum(3,5,6); // => 14
@@ -11,7 +11,7 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here 
+// Your code here
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
