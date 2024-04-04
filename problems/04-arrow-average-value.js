b1f4a393bfe9d89ff2f7cd6a830b2ee442f620d2
Heather Davidson <davidsonheather31@gmail.com>
2024-04-04 15:59:36 -0700

    first commit

--- problems/04-arrow-average-value.js
+++ problems/04-arrow-average-value.js
@@ -10,7 +10,7 @@ arrowAvgValue([2, 3, 7]); // => 4
 arrowAvgValue([100, 60, 64]); // => 74.66666666666667
 ***********************************************************************/
 
-// Your code here 
+// Your code here test comment
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
