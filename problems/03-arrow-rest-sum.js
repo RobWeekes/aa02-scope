f845f6556f6acbc8c23e152b9a5c29c9c60a3eb8
robweekes <weekes.robert@gmail.com>
2024-04-05 19:11:41 -0700

    done with prob 3

--- problems/03-arrow-rest-sum.js
+++ problems/03-arrow-rest-sum.js
@@ -11,21 +11,39 @@ arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
 ***********************************************************************/
 
-// Your code here
 const arrowRestSum = (...numsArr) => {
-  console.log(numsArr)
+  console.log(numsArr);
 
-  let sum = 0
+  let sum = 0;
   for (let i = 0; i < numsArr.length; i++){
-      sum += numsArr[i]
-      console.log(sum)
+      sum += numsArr[i];
+      console.log(sum);
   }
-  return sum
+  console.log(sum);
+  return sum;
 }
 
 arrowRestSum(3,5,6); // => 14
 arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
 arrowRestSum(0); // => 0
+
+
+// FIRST TRY NOT WORKING
+// const arrowRestSum = (...numsArr) => {
+//   console.log(numsArr)
+
+//   let sum = 0
+//   for (let i = 0; i < numsArr.length; i++){
+//       sum += numsArr[i]
+//       console.log(sum)
+//   }
+//   return sum
+// }
+
+// arrowRestSum(3,5,6); // => 14
+// arrowRestSum(1, 2, 3, 4, 5, 6, 7, 8, 9); // => 45
+// arrowRestSum(0); // => 0
+
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
 try {
