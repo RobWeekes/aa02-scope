11e71863a23106956912cbbccca4fb7f68032593
robweekes <weekes.robert@gmail.com>
2024-04-05 19:05:01 -0700

    done with prob 5

--- problems/05-arrow-mirror-array.js
+++ problems/05-arrow-mirror-array.js
@@ -10,27 +10,47 @@ arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 ***********************************************************************/
 
-// Your code here
 const arrowMirrorArray = (array) => {
-      let newArr = []
+  let newArr = [];
 
-        // console.log(newArr)
-      let reversedArr = array.reverse()
-      // console.log(reversedArr)
-newArr.push(reversedArr)
-console.log(newArr)
-newArr.push(array)
-console.log(newArr)
-      // let subArr = []
-      // subArr = newArr.push(reversedArr)
-      // console.log(subArr)
-    // newArr.reverse(array)
-      // console.log(newArr)
+  array.forEach((num) =>  {
+      newArr.push(num);
+      console.log(newArr);
+  })
 
+  console.log(newArr);
+  newArr.reverse();
+  console.log(newArr);
+
+  let combined = [...array, ...newArr];   // adds each element in sequence to new array 'combined'
+  console.log(combined);
+  return combined;
 }
 
 arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
 arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
+
+
+// FIRST TRY NOT WORKING, NEED TO PUSH 1 ELEMENT AT A TIME WITH FOREACH LOOP
+// const arrowMirrorArray = (array) => {
+//       let newArr = []
+
+//         // console.log(newArr)
+//       let reversedArr = array.reverse()
+//       // console.log(reversedArr)
+// newArr.push(reversedArr)
+// console.log(newArr)
+// newArr.push(array)
+// console.log(newArr)
+//       // let subArr = []
+//       // subArr = newArr.push(reversedArr)
+//       // console.log(subArr)
+//     // newArr.reverse(array)
+//       // console.log(newArr)
+// }
+
+// arrowMirrorArray([1,2,3]); // => [ 1, 2, 3, 3, 2, 1 ]
+// arrowMirrorArray(['a', 'b', 'c', 'd']); // => [ 'a', 'b', 'c', 'd', 'd', 'c', 'b', 'a' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
